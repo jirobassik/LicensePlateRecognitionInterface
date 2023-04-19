614ea85a8c50b8e3f0de1891f4d8716f54bd7fb9
Antonio <80766419+AntonioSivaha@users.noreply.github.com>
2023-04-19 20:18:41 +0300

    Completed model. Added dynamic tables in UI, but no logic.

--- profile_and_site_settings/admin.py
+++ profile_and_site_settings/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 
-from profile_and_site_settings.models import Profile
+from profile_and_site_settings.models import Profile, LicensePlates, WhiteList
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(LicensePlates)
+class UserLicensePlatesAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(WhiteList)
+class UserWhiteListAdmin(admin.ModelAdmin):
     pass
--- profile_and_site_settings/views.py
+++ profile_and_site_settings/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
 
-from profile_and_site_settings.models import Profile
+from profile_and_site_settings.models import Profile, WhiteList, LicensePlates
 
 
 class SettingsListView(generic.ListView):
     model = Profile
-    template_name = 'settings.html'
+    template_name = 'profile_and_site_settings/settings.html'
