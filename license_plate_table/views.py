4944e18d53ecd7823276185a324aba596996ff62
= <jirobassik@tut.by>
2023-05-10 21:33:15 +0300

    Update view table

--- license_plate_table/views.py
+++ license_plate_table/views.py
@@ -2,9 +2,20 @@ import django.db
 from django.shortcuts import render
 from license_plate_table.models import License_plate
 from utilities.data_repr.column_func import get_columns, init_dict_requests
-
+from profile_and_site_settings.models import WhiteList
 
 def view_data_table(request):
+    main_objs = License_plate.objects.all()
+    white_list = WhiteList.objects.all()
+    for item in main_objs:
+        lic_plate = item.license_plate
+        print('license', lic_plate)
+        for item_white in white_list:
+            print('white', item_white.license_plate)
+            if item_white.license_plate == lic_plate:
+                license_plate_obj = License_plate.objects.get(id=item.id)
+                license_plate_obj.user_name = item_white.user_name
+                license_plate_obj.save()
     main_objs = License_plate.objects.all()
     columns = get_columns()
     return render(request, 'license_plate_table/license_plate_table.html',
