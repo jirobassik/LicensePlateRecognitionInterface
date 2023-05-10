58eeceb5172c7c8ce22d4c24df3f864bf49a6ab3
Jen-Shen <anischenko2016@yandex.ru>
2023-05-10 15:39:02 +0300

    Add app license_recognition

--- license_recognition/views.py
+++ license_recognition/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 
+
 def view_license_recognition(request):
-    if request.method == 'POST' and request.FILES.get('image'):
-        image = request.FILES['image']
-        # Сохраняем файл на сервере, например, в директории media
-        with open(f'media/{image.name}', 'wb+') as destination:
-            for chunk in image.chunks():
-                destination.write(chunk)
+    if 'upload_photo' in request.POST:
+        if image := request.FILES.get('image', False):
+            # image = request.FILES.get('image', False)
+            print(True)
+            print(image)
+            # Сохраняем файл на сервере, например, в директории media
+            with open(f'static/image/{image.name}', 'wb+') as destination:
+                for chunk in image.chunks():
+                    destination.write(chunk)
 
     return render(request, 'license_recognition/license_recignition.html')
